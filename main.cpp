28d7c396b4250ace3d4706fe7bd3cf00f59ac905
Navi-nk <naval.kumar99@gmail.com>
2018-03-04 16:28:39 +0800

    unordered and closest search done

--- main.cpp
+++ main.cpp
@@ -15,48 +15,114 @@ std::unordered_map<long, std::unordered_map<long, std::vector<long>>> matrixMap;
 std::vector<long> searchOrdered(std::vector<long> inputArr)
 {
     if (matrixMap.find(inputArr[0]) == matrixMap.end() || inputArr.size() > matrix[0].size())
-            return {-1};
+        return {-1};
 
     std::vector<long> res;
     long i = 0;
-    
+
     std::unordered_map<long, std::vector<long>> innerMap = matrixMap[inputArr[0]];
     for (std::unordered_map<long, std::vector<long>>::iterator it = innerMap.begin(); it != innerMap.end(); it++)
     {
         std::vector<long> innerVec = it->second;
-        for(std::vector<long>::iterator it1 = innerVec.begin(); it1 != innerVec.end(); it1++)
+        for (std::vector<long>::iterator it1 = innerVec.begin(); it1 != innerVec.end(); it1++)
         {
-            long j=1,pos=*it1;
-            while(j<inputArr.size())
+            long j = 1, pos = *it1;
+            while (j < inputArr.size())
             {
-                if(matrix[it->first][pos+j] == inputArr[j])
+                if (matrix[it->first][pos + j] == inputArr[j])
                 {
                     j++;
                     continue;
                 }
                 break;
             }
-            if(j==inputArr.size())
+            if (j == inputArr.size())
             {
                 res.push_back(it->first);
                 break;
             }
         }
-
     }
-    if(res.size() == 0)
+    if (res.size() == 0)
         res.push_back(-1);
     return res;
 }
 
 std::vector<long> searchUnordered(std::vector<long> inputArr)
 {
+    if (inputArr.size() > matrix[0].size())
+        return {-1};
+    std::unordered_map<long, long> searchMap;
+    std::vector<long> res;
 
+    for (long i = 0; i < inputArr.size(); i++)
+    {
+        //if (matrixMap.find(inputArr[i]) == matrixMap.end())
+        //return {-1};
+        try
+        {
+            std::unordered_map<long, std::vector<long>> innerMap = matrixMap.at(inputArr[i]);
+            for (std::unordered_map<long, std::vector<long>>::iterator it = innerMap.begin(); it != innerMap.end(); it++)
+            {
+                searchMap[it->first]++;
+            }
+        }
+        catch (...)
+        {
+            return {-1};
+        }
+    }
+
+    for (std::unordered_map<long, long>::iterator it = searchMap.begin(); it != searchMap.end(); it++)
+    {
+        ///std::cout<<it->first<<" "<<it->second<<std::endl;
+        if (it->second == inputArr.size())
+        {
+            res.push_back(it->first);
+        }
+    }
+
+    if (res.size() == 0)
+        res.push_back(-1);
+
+    return res;
 }
 
 std::vector<long> searchClosest(std::vector<long> inputArr)
 {
+    if (inputArr.size() > matrix[0].size())
+        return {-1};
+    std::unordered_map<long, long> searchMap;
 
+    for (long i = 0; i < inputArr.size(); i++)
+    {
+        try
+        {
+            std::unordered_map<long, std::vector<long>> innerMap = matrixMap.at(inputArr[i]);
+            for (std::unordered_map<long, std::vector<long>>::iterator it = innerMap.begin(); it != innerMap.end(); it++)
+            {
+                searchMap[it->first]++;
+            }
+        }
+        catch (...)
+        {
+            //return {-1};
+        }
+    }
+
+    long max = 0;
+    long position = -1;
+    for (std::unordered_map<long, long>::iterator it = searchMap.begin(); it != searchMap.end(); it++)
+    {
+        ///std::cout<<it->first<<" "<<it->second<<std::endl;
+        if (it->second > max)
+        {
+            max = it->second;
+            position = it->first;
+        }
+    }
+
+    return {position};
 }
 void preprocessMatrix()
 {
@@ -109,11 +175,11 @@ void printMap()
 
 void printResult(std::vector<long> res)
 {
-    for(std::vector<long>::iterator it=res.begin(); it != res.end(); it++)
+    for (std::vector<long>::iterator it = res.begin(); it != res.end(); it++)
     {
-        std::cout<<*it<<" ";
+        std::cout << *it << " ";
     }
-    std::cout<<std::endl;
+    std::cout << std::endl;
 }
 
 std::unordered_map<std::string, std::vector<long> (*)(std::vector<long>)> funcMap = {
@@ -136,12 +202,12 @@ int main(int argc, char **argv)
     preprocessMatrix();
 
     printMap();
-    std::cout<<matrix[0].size()<<std::endl;
+    //std::cout<<matrix[0].size()<<std::endl;
     std::string line;
-    while (std::getline(std::cin, line)) 
+    while (std::getline(std::cin, line))
     {
-        line = std::regex_replace(line, std::regex("( ) +")," ");
-        line = std::regex_replace(line, std::regex("^ +| +$"),"");
+        line = std::regex_replace(line, std::regex("( ) +"), " ");
+        line = std::regex_replace(line, std::regex("^ +| +$"), "");
 
         std::cout << "Line Received: " << line << '\n';
 
@@ -184,8 +250,7 @@ int main(int argc, char **argv)
         }
         else
         {
-            std::cout<<"command not recognized"<<std::endl;
-
+            std::cout << "command not recognized" << std::endl;
         }
     }
 
